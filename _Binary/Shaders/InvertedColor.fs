2962679e4f6c244a9b76080510bc9acc99c7b87a
DennisSSDev <dennis.slavinsky@gmail.com>
2019-09-11 16:21:38 -0400

    Added Invert Shader

---
+++ _Binary/Shaders/InvertedColor.fs
@@ -0,0 +1,25 @@
+#version 330
+
+in vec3 Color;
+
+uniform vec3 SolidColor = vec3(-1,-1,-1);
+uniform int InvertColor; 
+
+out vec4 Fragment;
+
+void main()
+{
+	Fragment = vec4(Color,1);
+	
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
+		Fragment = vec4(SolidColor, 1);
+	else if(InvertColor > 0) {  
+		vec3 endColor = vec3(0,0,0);
+		endColor.r = 1 - Color.r;
+		endColor.g = 1 - Color.g;
+		endColor.b = 1 - Color.b;
+		Fragment = vec4(endColor, 1);
+	}
+
+	return;
+}
